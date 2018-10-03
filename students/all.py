088dd60965e8a1ba62cc65a7907f492377a1ad8c
aleenak123 <43809763+aleenak123@users.noreply.github.com>
2018-10-03 15:13:29 +0530

    Create all.py

---
+++ students/all.py
@@ -0,0 +1,10 @@
+filename="test.txt.txt"
+file=open(filename,'r')
+print(file.read())
+file.close()
+file=open(filename,'a')
+file.write("aleena k alex\n")
+file.close()
+file=open(filename,'r')
+print(file.readline())
+file.close()
